0e3cfde7868753a526ef8159491ebd14791d709a
aleksi_kistauri <lchkhetiani@systemcorp.ai>
2021-01-23 05:00:23 +0400

    depthwise conv block

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SplitSR
-SplitSR: An End-to-End Approach to Super-Resolution on Mobile Devices (Unofficial Implementation)
+Unofficial implementation of [SplitSR: An End-to-End Approach to Super-Resolution on Mobile Devices](https://arxiv.org/abs/2101.07996)
+
+## Progress
+- DepthWise Convolution Block is implemented.
---
+++ modules/block.py
@@ -0,0 +1,35 @@
+import torch
+from torch import nn
+
+class SplitSRBlock(nn.Module):
+    def __init__(self, channels, kernel, alpha):
+        super(SplitSRBlock, self).__init__()
+        self.alpha = alpha
+        self.channels = channels
+        self.conv = nn.Conv2d(in_channels=int(self.channels*self.alpha), out_channels=int(self.channels*self.alpha), kernel_size=kernel, stride=1, padding=kernel//2)
+        self.batchnorm = nn.BatchNorm2d(int(self.channels*self.alpha))
+        self.relu = nn.ReLU(inplace=True)
+
+    
+
+    def forward(self, x):
+        active, passive = x[:, :int(self.channels*self.alpha)], x[:, int(self.channels*self.alpha):]
+        active = self.conv(active)
+        active = self.batchnorm(active)
+        active = self.relu(active)
+        x = torch.cat([active, passive], dim=1)
+        return x
+
+if __name__ == '__main__':
+    block_alphas = [0.125, 0.256, 0.500, 1.000]
+
+    for alpha in block_alphas:
+        block = SplitSRBlock(128, 3, alpha)
+
+        x = torch.randn(1, 256, 112, 112)
+        y = block(x)
+
+        assert y.shape == x.shape
+
+        params = sum(p.numel() for p in block.parameters() if p.requires_grad)
+        print(f'Total params for block with α={alpha}: {params}')
